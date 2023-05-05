75e2db8e7108c6454f18187fed46b2acb0851a91
AadityaJujagar <aadityajujagar@gmail.com>
2023-05-05 14:49:55 +0530

    object cloning learnt

---
+++ Front-End/Core JS/javascript class 2/object cloning/index.js
@@ -0,0 +1,31 @@
+// JavaScript posseses the automated garbage value
+// collecting management system called as Garbase Collector
+
+// object cloaning
+let rectangle1 = {
+  len: 7,
+  bre: 6,
+  ht: 10,
+};
+let rectangle2 = {};
+
+// cloning done here
+for (let key in rectangle1) {
+  rectangle2[key] = rectangle1[key];
+}
+// pringing the clone
+for (let key in rectangle2) {
+  console.log(key, rectangle2[key]);
+}
+
+// another method
+let rectangle3 = Object.assign({}, rectangle1, rectangle2);
+for (let key in rectangle3) {
+  console.log(key, rectangle3[key]);
+}
+
+// another method
+let rectangle4 = { ...rectangle3 };
+for (let key in rectangle4) {
+  console.log(key, rectangle4[key]);
+}
