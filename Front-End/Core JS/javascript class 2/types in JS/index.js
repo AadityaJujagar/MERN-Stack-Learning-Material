e23dd0e328326af11e668a6638354c47f602ccec
AadityaJujagar <aadityajujagar@gmail.com>
2023-05-05 13:57:09 +0530

    types, for-in for-of loop learnt

--- Front-End/Core JS/javascript class 2/types in JS/index.js
+++ Front-End/Core JS/javascript class 2/types in JS/index.js
@@ -1,6 +1,41 @@
-// Primitive Types
+// primitive type
+// premitives are copied by thier values
 let a = 5;
 let b = a;
 a++;
 console.log(a);
 console.log(b);
+
+// reference type
+// references are copied by their values
+let c = { value: 3 };
+let d = c;
+c.value++;
+console.log(c.value);
+console.log(d.value);
+
+// iterating through objects
+// for-in loop
+let rectangle = {
+  len: 44,
+  bre: 55,
+};
+for (let key in rectangle) {
+  console.log(key, rectangle[key]);
+  // console.log(key,value);
+}
+
+// for-of loop
+for (let key of Object.keys(rectangle)) {
+  console.log(key, rectangle[key]);
+}
+for (let key of Object.entries(rectangle)) {
+  console.log(key, rectangle[key]);
+}
+
+// finding if object property is in object or not
+if ("color" in rectangle) {
+  console.log("color is in rectangle");
+} else {
+  console.log("color is not in rectangle");
+}
