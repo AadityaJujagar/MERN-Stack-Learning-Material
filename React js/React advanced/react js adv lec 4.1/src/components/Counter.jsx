36d53530457cd70b84553996d0e77ffffc942a3c
AadityaJujagar <aadityajujagar@gmail.com>
2023-09-01 11:15:22 +0530

    Redux in React learnt

---
+++ React js/React advanced/react js adv lec 4.1/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "../redux/slices/CounterSlice";
+
+function Counter() {
+  const count = useSelector((state) => state.counter.value);
+  // for calling the inc/dec functions
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <div>{count}</div>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+    </div>
+  );
+}
+
+export default Counter;
